c6e2a7d9a54c46fb22dba0c28de687107feafdc5
Efe Yegitoğlu <76888997+EfeYegitoglu@users.noreply.github.com>
2022-04-27 21:16:30 +0300

    taxiApp

---
+++ app/src/main/java/com/efeyegitoglu/taxiapp/MapsActivity.kt
@@ -0,0 +1,102 @@
+package com.efeyegitoglu.taxiapp
+
+import android.Manifest
+import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.efeyegitoglu.taxiapp.databinding.ActivityMapsBinding
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private val tag:String = "Info"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        val apiKey:String = getString(R.string.api_key)
+
+        if (!Places.isInitialized()){
+            Places.initialize(this,apiKey)
+        }
+
+        val placesClient:PlacesClient = Places.createClient(this)
+
+
+        // Initialize the AutocompleteSupportFragment.
+        val autocompleteFragment =
+            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                    as AutocompleteSupportFragment
+
+        autocompleteFragment.setLocationBias(RectangularBounds.newInstance(
+            LatLng(41.0052367,28.8720966),
+            LatLng(41.0052367,28.8720966)
+        ))
+
+        autocompleteFragment.setCountries("TR")
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+            }
+
+            override fun onError(status: Status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: $status")
+            }
+        })
+
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        /*// Add a marker in Sydney and move the camera
+        val turkey = LatLng(41.0052367, 28.8720966)
+        mMap.addMarker(MarkerOptions().position(turkey).title("Marker in İstanbul"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(turkey))*/
+
+        mMap = googleMap
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        mMap.isMyLocationEnabled = true
+    }
+}
